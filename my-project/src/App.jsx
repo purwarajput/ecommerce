fcb46091c43c9fbab9b7635a43b32383f1e04ff7
Purwa <s.purwarajput@gmail.com>
2025-07-25 10:18:41 +0530

    updated

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -12,25 +12,33 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import { ProductZoom } from './components/ProductZoom';
 import { IoCloseCircleSharp } from "react-icons/io5";
+import { ProductDetailsComponent } from './components/ProductDetails';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+
 
 const MyContext = createContext();
 function App() {
-   const [openProductDetailsModal, setOpenProductDetailsModal] = useState(true);
-   const [maxWidth, setMaxWidth] = React.useState('lg');
-   const [fullWidth, setFullWidth] = React.useState(true);
+   const [openProductDetailsModal, setOpenProductDetailsModal] = useState(false);
+   const [maxWidth, setMaxWidth] = useState('lg');
+   const [fullWidth, setFullWidth] = useState(true);
 
-  const handleClickOpenProductDetailsModal = () => {
-    setOpenProductDetailsModal(true);
-  };
+   const [openCartPanel, setOpenCartPanel] = useState(false);
 
   const handleCloseProductDetailsModal = () => {
     setOpenProductDetailsModal(false);
   };
 
+  const toggleCartPanel = (newOpen) => () => {
+    setOpenCartPanel(newOpen);
+  };
+
   const values = {
-
-
-  }
+    setOpenProductDetailsModal,
+    setOpenCartPanel,
+    toggleCartPanel,
+    openCartPanel
+  };
 
   return (
     <>
@@ -40,7 +48,9 @@ function App() {
       <Routes>
         <Route path={"/"} exact={true} element={<Home />} />
         <Route path={"productListing"} exact={true} element={<ProductListing/>} />
-        <Route path={"product/:id"} exact={true} element={<ProductDetails/>} />
+        <Route path={"/product/:id"} exact={true} element={<ProductDetails/>} />
+        <Route path={"/login"} exact={true} element={<Login/>} />
+        <Route path={"/register"} exact={true} element={<Register/>} />
       </Routes>
       <Footer />
       </MyContext.Provider>
@@ -57,16 +67,24 @@ function App() {
       >
         <DialogContent>
          <div className='flex items-center w-full productDetailsModalContainer relative'>
-          <Button className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000] !absolute top-[0px] right-[0px]' onClick={handleCloseProductDetailsModal}><IoCloseCircleSharp/></Button>
-          <div className='col1 w-[40%]'>
+          <Button className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000] !absolute top-[15px] right-[15px] !bg-[#f1f1f1]' onClick={handleCloseProductDetailsModal}><IoCloseCircleSharp className='text-[20px]'/></Button>
+          <div className='col1 w-[40%] px-3'>
             <ProductZoom/>
+          </div>
+
+          <div className='col2 w-[60%] py-8 px-8 pr-16 productContent'>
+            <ProductDetailsComponent/>  
           </div>
          </div>
         </DialogContent>
       </Dialog>
+
+        
     </>
   );
 }
 
 export default App;
+
+export {MyContext}
 
